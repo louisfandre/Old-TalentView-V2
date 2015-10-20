b4f213941727482fd0137b4ccf2ad5a8bf9f2c96
Louis ANDRE <louisfandre@gmail.com>
2015-10-20 16:08:22 +0200

    Modif Wrapper et Js scroll

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,31 +2,56 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+$( document ).ready(function() {
+
+    var titles = [
+        'Attirez des candidats',
+        'Présentez votre entreprise',
+        'Visualisez les meilleurs profils',
+        'Recrutez la perle rare'
+    ];
+
+    $(window).scroll(function(){
+        var pos = $(this).scrollTop();
+        
+        if(pos < 20) {
+            $('.top').slideUp().text(titles[0]);
+        }
+        if(pos > 20) {
+            $('.top').slideDown();
+        }
+        if(pos > 60) {
+            $('.top').text(titles[1]);
+        }
+        if(pos > 100) {
+            $('.top').text(titles[2]);
+        }
+        if(pos > 140) {
+            $('.top').text(titles[3]);
+        }
+    });
+
+    $(window).on('scroll', function(){
+        if ($(this).scrollTop()>500){
+           $(".navbar").css("background-color","rgba(54,53,55,0.7)"); 
+        } else {
+           $(".navbar").css("background-color","transparent"); 
+        }
+
+        
+    });
 
 
-var titles = [
-    'Attirez des candidats',
-    'Présentez votre entreprise',
-    'Visualisez les meilleurs profils',
-    'Recrutez la perle rare'
-];
-
-$(window).scroll(function(){
-    var pos = $(this).scrollTop();
-    
-    if(pos < 20) {
-        $('.top').slideUp().text(titles[0]);
-    }
-    if(pos > 20) {
-        $('.top').slideDown();
-    }
-    if(pos > 60) {
-        $('.top').text(titles[1]);
-    }
-    if(pos > 100) {
-        $('.top').text(titles[2]);
-    }
-    if(pos > 140) {
-        $('.top').text(titles[3]);
-    }
-});
+    $('a[href*=#]:not([href=#])').click(function() {
+       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+         var target = $(this.hash);
+         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+           $('html,body').animate({
+             scrollTop: target.offset().top
+           }, 1000);
+           return false;
+         }
+       }
+     });
+ });
