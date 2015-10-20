4d35bedf6fe3102aa820b06e4d696b6ee0abc64f
Louis ANDRE <louisfandre@gmail.com>
2015-10-20 15:36:02 +0200

    Ajout Wrapper

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,32 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+
+var titles = [
+    'Attirez des candidats',
+    'Présentez votre entreprise',
+    'Visualisez les meilleurs profils',
+    'Recrutez la perle rare'
+];
+
+$(window).scroll(function(){
+    var pos = $(this).scrollTop();
+    
+    if(pos < 20) {
+        $('.top').slideUp().text(titles[0]);
+    }
+    if(pos > 20) {
+        $('.top').slideDown();
+    }
+    if(pos > 60) {
+        $('.top').text(titles[1]);
+    }
+    if(pos > 100) {
+        $('.top').text(titles[2]);
+    }
+    if(pos > 140) {
+        $('.top').text(titles[3]);
+    }
+});
